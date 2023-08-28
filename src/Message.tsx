dbaf5b156098c1adbc13ea6fc9447378b56ad39a
leeool <lgonsalez02@gmail.com>
2023-08-27 21:37:34 -0300

    🎨 feat: clean up unused code

--- src/Message.tsx
+++ src/Message.tsx
@@ -9,12 +9,12 @@ interface IMessageChat {
 
 const Message = ({ handleOption, ...message }: IMessage & IMessageChat) => {
   const { body, sender } = message
-  const optionRefs = React.useRef<HTMLDivElement[]>([])
+  const optionRefs = React.useRef<HTMLButtonElement[]>([])
 
   const handleChooseOption = async (e: React.MouseEvent, option: IOption) => {
-    handleOption(option)
 
-    if (!(e.currentTarget instanceof HTMLDivElement)) return
+    if (!(e.currentTarget instanceof HTMLButtonElement)) return
+    handleOption(option)
 
     const chosenOption = optionRefs.current.find(el => el === e.currentTarget)
 
@@ -29,7 +29,6 @@ const Message = ({ handleOption, ...message }: IMessage & IMessageChat) => {
   return (
     <Container
       data-sender={sender}
-      as={motion.div}
       className="messageContainer"
     >
       {body && <MessageBody data-sender={sender} className='messageBody'>
@@ -44,7 +43,7 @@ const Message = ({ handleOption, ...message }: IMessage & IMessageChat) => {
               key={String(option.body)}
               ref={el => optionRefs.current[index] = el!}
               onClick={(e) => handleChooseOption(e, option)}
-              as={motion.div}
+              as={motion.button}
               initial={{ y: body ? "-120%" : "120%" }}
               animate={{ y: "0" }}
               transition={{ delay: index * 0.3, type: "spring", duration: 1 }}
